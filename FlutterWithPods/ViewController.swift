d62e1b0f2900da43e7024dc0db449b02d7efc40d
takecian <takecian@gmail.com>
2018-12-25 15:34:36 +0900

    Show FlutterViewController from native

--- FlutterWithPods/ViewController.swift
+++ FlutterWithPods/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import Flutter
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton(type: .custom)
+        button.addTarget(self, action: #selector(handleButtonAction), for: .touchUpInside)
+        button.setTitle("Press me", for: .normal)
+        button.frame = CGRect(x: 80.0, y: 210.0, width: 160.0, height: 40.0)
+        button.backgroundColor = UIColor.blue
+        self.view.addSubview(button)
     }
 
-
+    @objc func handleButtonAction() {
+        let flutterViewController = FlutterViewController()
+        self.present(flutterViewController, animated: false, completion: nil)
+    }
 }
-
